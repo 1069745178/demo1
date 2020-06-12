d548e0894f71aec4df0036f87d9fca23b8ea14b7
ming <ming@126.com>
2020-06-12 17:26:53 +0800

    新增角色模块

--- src/main/java/cn/lym/springboot/controller/IndexController.java
+++ src/main/java/cn/lym/springboot/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
-
+    //嘻嘻嘻嘻嘻嘻
     @RequestMapping("/index")
     @ResponseBody
     public String index(){
