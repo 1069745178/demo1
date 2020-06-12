4e674ae89dd774190a2f1f641e904fada0d1c4b8
ming <ming@126.com>
2020-06-12 16:01:41 +0800

    初始化导入项目

---
+++ src/main/java/cn/lym/springboot/App.java
@@ -0,0 +1,15 @@
+package cn.lym.springboot;
+
+import cn.lym.springboot.customproperties.MyCustomProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(MyCustomProperties.class)
+public class App {
+    public static void main(String[] args) {
+
+        SpringApplication.run(App.class,args);
+    }
+}
